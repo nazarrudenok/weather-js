978780d93d57f1b47048d9df4b674e8242288c88
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2023-07-16 22:24:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
+// function info(el) {
+//     el.style.width = '150px';
+//     el.style.height = '25px';
+//     el.style.borderRadius = '15px';
+//     el.style.padding = '15px';
+//     el.innerHTML = `<p class="info-text">Ультрафіолетове випромінювання</p>`;
+// }
+
+// Зберігаємо початкові значення тексту і стилів
+var initialText = '';
+var initialStyles = {};
+
+function info(el) {
+  // Перевіряємо, чи вже змінено текст і стилі
+  if (initialText === '' && Object.keys(initialStyles).length === 0) {
+    // Зберігаємо початкові значення
+    initialText = el.innerHTML;
+    initialStyles = {
+      width: el.style.width,
+      height: el.style.height,
+      borderRadius: el.style.borderRadius,
+      padding: el.style.padding
+    };
+
+    // Змінюємо стилі
+    el.style.width = '150px';
+    el.style.height = '25px';
+    el.style.borderRadius = '15px';
+    el.style.padding = '15px';
+    el.innerHTML = `<p class="info-text">Ультрафіолетове випромінювання</p>`;
+  } else {
+    // Повертаємо початкові значення
+    el.style.width = initialStyles.width;
+    el.style.height = initialStyles.height;
+    el.style.borderRadius = initialStyles.borderRadius;
+    el.style.padding = initialStyles.padding;
+    el.innerHTML = initialText;
+
+    // Очищаємо початкові значення
+    initialText = '';
+    initialStyles = {};
+  }
+}
+
+
+
 const URL = 'https://api.weatherapi.com/v1/forecast.json?key=e43642ed5f89487c97c143549230706&q=Lviv&lang=uk&days=5';
 
 const xhr = new XMLHttpRequest();
@@ -57,7 +103,7 @@ xhr.onload = () => {
     temp.innerHTML = `<p class="temp">${response['current']['condition']['text']} ${Math.round(response['current']['temp_c'])}°</p>`;
 
     var temp = document.getElementsByClassName('uv-container')[0];
-    temp.innerHTML = `<p class="uv">УФ: ${response['forecast']['forecastday'][0]['day']['uv']}</p>`;
+    temp.innerHTML = `<img src="styles/images/info.png" alt="" class="info"><p class="uv">УФ: ${response['forecast']['forecastday'][0]['day']['uv']}</p>`;
 
     var otherData = document.getElementsByClassName('other-data-status-container')[0];
     otherData.innerHTML = `<p class="other-data-status">${response['current']['humidity']}%</p>`;
@@ -88,32 +134,32 @@ xhr.onload = () => {
     var formattedTime = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
     otherData.innerHTML = `<p class="other-data-status">${formattedTime}</p>`;
 
+
+
     const today = new Date();
-    const dayNames = ["понеділок", "вівторок", "середа", "четвер", "п’ятниця", "субота", "неділя"];
-    
     const shortDayNames = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"];
     
-    for (let i = 0; i < 3; i++) {
-        const dayName = shortDayNames[(today.getDay() + i) % 7];
-
-        var status = response['forecast']['forecastday'][i]['day']['condition']['text'].toLowerCase();
-        var statusFor = response['forecast']['forecastday'][i]['day']['condition']['text'];
-        var maxTemp = Math.round(response['forecast']['forecastday'][i]['day']['maxtemp_c']);
-        var minTemp = Math.round(response['forecast']['forecastday'][i]['day']['mintemp_c']);
+    for (let i = -1; i < 2; i++) {
+        const dayName = shortDayNames[(today.getDay() + i + 7) % 7];
+    
+        var status = response['forecast']['forecastday'][i + 1]['day']['condition']['text'].toLowerCase();
+        var statusFor = response['forecast']['forecastday'][i + 1]['day']['condition']['text'];
+        var maxTemp = Math.round(response['forecast']['forecastday'][i + 1]['day']['maxtemp_c']);
+        var minTemp = Math.round(response['forecast']['forecastday'][i + 1]['day']['mintemp_c']);
         var maxMinTemp = maxTemp + '° / ' + minTemp + '°';
-        console.log(maxMinTemp);
-
+    
         var statusImg = document.getElementsByClassName('forecast-day-img');
         if (status.includes("хмарність") || status.includes("хмарно")) {
             document.querySelector(".forecast-days").innerHTML += `<div class="forecast-day"><img src="styles/images/weather-icons/cloudy.png" class="forecast-day-img"><p class="forecast-day-name">${dayName}</p><p class="forecast-day-status">${statusFor}</p><p class="forecast-day-temp">${maxMinTemp}</p></div>`;
         } else if (status.includes("дощ") || status.includes("злива")) {
             document.querySelector(".forecast-days").innerHTML += `<div class="forecast-day"><img src="styles/images/weather-icons/rain.png" class="forecast-day-img"><p class="forecast-day-name">${dayName}</p><p class="forecast-day-status">${statusFor}</p><p class="forecast-day-temp">${maxMinTemp}</p></div>`;
-        } else if (status.includes("сонячно") || status.includes("ясно")) {statusFor
+        } else if (status.includes("сонячно") || status.includes("ясно")) {
             document.querySelector(".forecast-days").innerHTML += `<div class="forecast-day"><img src="styles/images/weather-icons/sun.png" class="forecast-day-img"><p class="forecast-day-name">${dayName}</p><p class="forecast-day-status">${statusFor}</p><p class="forecast-day-temp">${maxMinTemp}</p></div>`;
         } else {
             document.querySelector(".forecast-days").innerHTML += `<div class="forecast-day"><img src="styles/images/weather-icons/cloudy.png" class="forecast-day-img"><p class="forecast-day-name">${dayName}</p><p class="forecast-day-status">${statusFor}</p><p class="forecast-day-temp">${maxMinTemp}</p></div>`;
         }
     }
+    
 }
 
 xhr.send();
